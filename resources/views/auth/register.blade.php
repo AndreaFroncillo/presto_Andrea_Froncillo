efe5c92e2ebc68f34b351e9903b5183bc026bebe
AndreaFroncillo <andreafroncillo0@gmail.com>
2025-09-12 01:44:37 +0200

    User Story 1 completata

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,38 @@
+<x-layout>
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-12 text-center">
+                <h1 class="display-4 pt-5">Registrati</h1>
+            </div>
+        </div>
+    </div>
+
+    <x-error-messages />
+
+    <div class="row justify-content-center align-items-center height-custom w-100">
+        <div class="col-12-col-md-6">
+            <form method="POST" action="{{route('register')}}" class="bg-body-tertiary shadow rounded p-5 pe-5 mx-5">
+                @csrf
+                <div class="mb-3">
+                    <label for="name" class="form-label">Nome:</label>
+                    <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}">
+                </div>
+                <div class="mb-3">
+                    <label for="registerEmail" class="form-label">Indirizzo email:</label>
+                    <input type="email" class="form-control" id="registerEmail" name="email" value="{{ old('email') }}">
+                </div>
+                <div class="mb-3">
+                    <label for="password" class="form-label">Password:</label>
+                    <input type="password" class="form-control" id="password" name="password" value="{{ old('password') }}">
+                </div>
+                <div class="mb-3">
+                    <label for="password_confirmation" class="form-label">Conferma password:</label>
+                    <input type="password" class="form-control" id="password" name="password_confirmation">
+                </div>
+                <div class="d-flex justify-content-center">
+                    <button type="submit" class="btn btn-dark">Registrati</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</x-layout>
---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,45 @@
+<footer class="bg-body-tertiary text-center">
+  <!-- Grid container -->
+  <div class="container p-4 pb-0">
+    <!-- Section: Social media -->
+    <section class="mb-4">
+      <!-- Facebook -->
+      <a
+      data-mdb-ripple-init
+        class="btn text-white btn-floating m-1 fbi-custom"
+        href="https://www.facebook.com"
+        role="button"
+        ><i class="bi bi-facebook"></i
+      ></a>
+
+      <!-- Twitter -->
+      <a
+        data-mdb-ripple-init
+        class="btn text-white btn-floating m-1 bg-dark"
+        href="https://x.com"
+        role="button"
+        ><i class="bi bi-twitter-x"></i
+      ></a>
+
+
+      <!-- Instagram -->
+      <a
+        data-mdb-ripple-init
+        class="btn text-white btn-floating m-1 insta-i-custom"
+        href="https://www.instagram.com"
+        role="button"
+        ><i class="bi bi-instagram"></i
+      ></a>
+
+    </section>
+    <!-- Section: Social media -->
+  </div>
+  <!-- Grid container -->
+
+  <!-- Copyright -->
+  <div class="text-center p-3 footer-custom">
+    © 2025 Copyright:
+    <a class="text-body" href="#">Presto.it</a>
+  </div>
+  <!-- Copyright -->
+</footer>
