b2bbf444b7b43a26819e046b2909e59edad034af
AndreaFroncillo <andreafroncillo0@gmail.com>
2025-09-17 23:30:48 +0200

    User Story 10 completata

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -60,6 +60,15 @@
                 </li>
                 @endauth
             </ul>
+            <form class="d-flex ms-auto" role="search" action="{{route('article.search')}}" method="GET">
+                @csrf
+                <div class="input-group">
+                    <input type="search" name="query" class="form-control" placeholder="Search" aria-label="search">
+                    <button type="submit" class=" input-group-text btn btn-outline-success" id="basic-addon2">
+                        Search
+                    </button>
+                </div>
+            </form>
         </div>
     </div>
 </nav>
